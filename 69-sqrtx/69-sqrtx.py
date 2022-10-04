26e15b0e6544f44c83bd48dd7a45c9feec1a7367
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-04 19:21:07 +0530

    Time: 67 ms (54.47%), Space: 13.8 MB (95.68%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,8 +1,23 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x==0:    return 0
-        if x==1 or x==2 or x==3 :    return 1
-        if x==4 or x==5 or x==6 or x==7:    return 2
-        for i in range(2,(x//2)+1):
-            if x>=(i*i): pass
-            else:   return i-1
+        # For x = 0 and x = 1
+        if x <= 1: return x
+        
+        start = 2
+        # We know that a square root of any number is at most the mid, not more than that
+        end = x // 2
+        
+        # Apply Binary Search since range 1 to x is in sorted order
+        while start <= end:
+            mid = start + (end - start)  // 2
+            
+            square = mid * mid
+            # If square of a number is equal to x, we got the square root
+            if square == x: return mid
+            # If square of a number if less than x, go for a bigger number
+            if square < x: start = mid + 1
+            # If square of a number if more than x, go for a smaller number
+            else: end = mid - 1
+        
+        # If the number is not a perfect square, return the value "end"
+        return end
