25d5f9b3c3171e1c44f32da8586eca6b391d377c
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-04 19:08:38 +0530

    Time: 4628 ms (9.04%), Space: 13.9 MB (56.25%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,3 +1,7 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x**0.5)
+        if x==0:    return 0
+        if x==1:    return 1
+        for i in range(x+1):
+            if x>=(i*i): pass
+            else:   return i-1
