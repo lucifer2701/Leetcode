9688d5ea8990984f11e655224ec3d731651dac87
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-09 15:59:29 +0530

    Time: 235 ms (61.18%), Space: 16.7 MB (49.13%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,6 +1,6 @@
-from collections import Counter
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        x=Counter(nums)
-        for ele in x:
-            if x[ele]==1:   return ele
+        ans=0
+        for i in range(len(nums)):
+            ans^=nums[i]
+        return ans
