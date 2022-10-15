868f57956cac64787879a0b960780a633d63ccee
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-15 15:43:32 +0530

    Time: 59 ms (28.39%), Space: 14 MB (22.95%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,6 +1,12 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        li=list(s.split(" "))
-        if len(pattern)!=len(li):   return 0
-        return len(set(pattern)) == len(set(li)) == len(set(zip(pattern,li)))
+        li1=list(s.split(" "))
+        li2=[pattern[i] for i in range(len(pattern))]
+        if len(li2)!=len(li1):   return 0
+        mp=[]
+        for i in range(len(li1)):
+            temp=(li1[i],li2[i])
+            mp.append(temp)
+            temp=()
+        return len(set(li1)) == len(set(li2)) == len(set(mp))
         
