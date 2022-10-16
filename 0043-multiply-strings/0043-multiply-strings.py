9e74161b418df4ce68c90c6296f1bfc5aa237699
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-16 22:53:54 +0530

    Time: 60 ms (71.56%), Space: 13.9 MB (29.39%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,3 +1,8 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(eval(num1+'*'+num2))
+        def str2int(num):
+            val=0
+            for i in num:
+                val=val*10+(ord(i)-ord('0'))
+            return val
+        return str(str2int(num1)*str2int(num2))
