e8ef9bca3223d959068ff6c953c32c4cb638e512
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-17 23:32:20 +0530

    Time: 38 ms (96.77%), Space: 13.8 MB (97.63%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
@@ -1,14 +1,9 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        x=dict()
-        for ele in words:
-            x[ele]=ele
-        ans=[]
-        for ele in words:
-            for w in x:
-                if ele in x[w] and ele!=w:
-                    ans.append(ele)
-        ans=set(ans)
-        return list(ans)
-                    
-                
+        res=[]
+        total="_".join(words)
+        for i in words:
+            ocur=total.count(i)
+            if(ocur>1):
+                res.append(i)
+        return res
