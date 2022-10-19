9a8cca2c9d68f88e72131e1b938565edd9756516
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-19 18:29:13 +0530

    Time: 80 ms (59.86%), Space: 14 MB (54.91%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,16 +1,24 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-	    dp = ["" for i in range(n+1)]
-	    dp[1] = "1"
-	    for i in range(2, n+1):
-		    last_c = dp[i-1][0]
-		    count = 0
-		    for c in dp[i-1]:
-			    if c == last_c:
-				    count += 1
-			    else:
-				    dp[i] += str(count) + last_c
-				    last_c = c
-				    count = 1
-		    dp[i] += str(count) + last_c
-	    return dp[n]        
+        count = [str(i) for i in range(0,10)]
+        
+        if n==1:
+            return "1"
+        i=1
+        s="1"
+        while(i<n):
+            s2=""
+            curr=s[0]
+            c=0
+            for ch in s:
+                if curr==ch:
+                    c+=1
+                else:
+                    s2+=str(c)+curr
+                    curr = ch
+                    c=1
+            s2+=str(c)+curr
+            s=s2
+            
+            i+=1
+        return s2
