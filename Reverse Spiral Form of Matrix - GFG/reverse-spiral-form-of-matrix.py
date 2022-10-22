36fa35e10f188ea521f35aa40f2e9536f6286c55
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-22 08:38:58 +0530

    Added solution - LeetHub

--- Reverse Spiral Form of Matrix - GFG/reverse-spiral-form-of-matrix.py
+++ Reverse Spiral Form of Matrix - GFG/reverse-spiral-form-of-matrix.py
@@ -1,35 +1,46 @@
+#User function Template for python3
 class Solution:
 	def reverseSpiral(self, R, C, a):
 		# code here
-		out = []
-		if R == 0:
-		    return out
-		    
-		r_b = 0
-		c_b = 0
-		r_e = R-1
-		c_e = C-1
-		
-		while (r_b <= r_e) and (c_b <= c_e):
-		    for i in range(c_b, c_e+1):
-		        out.append(a[r_b][i])
-		    
-		    r_b+=1
-		    for i in range(r_b, r_e+1):
-		        out.append(a[i][c_e])
-		    c_e-=1
-		    if (r_b <=r_e):
-		        for i in range(c_e, c_b-1, -1):
-		            out.append(a[r_e][i])
-		        
-		        r_e -=1
-		        
-		    if (c_b <= c_e):
-		        for i in range(r_e, r_b-1, -1):
-		            out.append(a[i][c_b])
-		        c_b+=1
-		        
-        return out[::-1]
+		t=n=R*C
+		r,c=0,0
+		rn,cn=R,C
+		li=[];ans=[]
+		if rn==1:
+		    ans=a[0]
+		    ans.reverse()
+		    return ans
+		if cn==1:
+		    for i in range(rn):
+		        ans.append(a[i][0])
+		    ans.reverse()
+		    return ans
+		while t!=0:
+		    for i in range(c,cn):
+		        li.append(a[r][i])
+		        t-=1
+		    if t==0:    break
+		    r+=1
+		    for i in range(r,rn):
+		        li.append(a[i][cn-1])
+		        t-=1
+		    if t==0:    break
+		    cn-=1
+		    for i in range(cn-1,c-1,-1):
+		        li.append(a[rn-1][i])
+		        t-=1
+		    if t==0:    break
+		    rn-=1
+		    for i in range(rn-1,r-1,-1):
+		        li.append(a[i][c])
+		        t-=1
+		    if t==0:    break
+		    c+=1
+		ans=li[:n]
+		ans.reverse()
+        return ans
+
+
 
 #{ 
  # Driver Code Starts
