c7f9bcaf0fe7fd9c66df1530e1c90c7f092eb556
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-22 21:43:11 +0530

    Time: 304 ms (32.03%), Space: 15.8 MB (62.96%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -4,17 +4,12 @@ class Solution:
         s=list(s)
         for i in range(len(s)):
             if s[i]=='(':
-                stack.append('(')
+                stack.append(i)
             elif s[i]==')':
                 if len(stack)>0:
                     stack.pop(0)
                 else:
                     s[i]=""
-        x=len(stack)
-        i=len(s)-1
-        while x!=0:
-            if s[i]=='(':
-                s[i]=""
-                x-=1   
-            i-=1
+        for ele in stack:
+            s[ele]=""
         return "".join(s)          
