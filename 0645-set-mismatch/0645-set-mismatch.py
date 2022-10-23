e49cf81a31c1ca8f97afbbf32b4b897827a0148a
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-23 17:56:23 +0530

    Time: 585 ms (19.05%), Space: 15.8 MB (60.69%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,8 +1,7 @@
-from collections import Counter
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         ans=[];t=[]
-        dic=Counter(nums)
+        dic=collections.Counter(nums)
         for i in range(1,len(nums)+1):
             if i not in dic:    ans.append(i)
             elif dic[i]>1:  t.append(i)
