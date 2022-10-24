6219be8457b4148004853ce591185b7504b918ef
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-24 23:09:15 +0530

    Time: 174 ms (28.88%), Space: 18.5 MB (27.30%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -0,0 +1,27 @@
+class Solution:
+    def find(self, node):
+        while node.left:
+            node = node.left
+        return node
+    
+    def solve(self, node, key):
+        if not node:
+            return None
+        if node.val < key:
+            node.right = self.solve(node.right, key)
+        elif node.val > key:
+            node.left = self.solve(node.left, key)
+        elif node.val == key:
+            if not node.left:
+                return node.right
+            elif not node.right:
+                return node.left
+            else:
+                mn = self.find(node.right)
+                #print(mn.val)
+                node.val = mn.val
+                node.right = self.solve(node.right, node.val)
+        return node
+    
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        return self.solve(root, key)
