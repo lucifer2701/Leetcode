05d678f96979af6affd7bf51064bf974be08e605
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-27 18:57:43 +0530

    Added solution - LeetHub

---
+++ Fitting The Array - GFG/fitting-the-array.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+    def isFit (self,arr, brr, n) : 
+        #Complete the function
+        arr.sort()
+        brr.sort()
+        for i in range(n):
+            if arr[i]>brr[i]:
+                return 0
+        return 1
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+for _ in range(0,int(input())):
+    n = int(input())
+    arr = list(map(int, input().strip().split()))
+    brr = list(map(int, input().strip().split()))
+    a=Solution()
+    ans = a.isFit(arr, brr, n)
+    if(ans == True):
+        print("YES")
+    else:
+        print("NO")
+# } Driver Code Ends
