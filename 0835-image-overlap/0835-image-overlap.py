e1e321393a1747d44f978a404623a00fc608ac1f
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-27 14:31:06 +0530

    Time: 1877 ms (30.41%), Space: 14.2 MB (67.84%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.py
+++ 0835-image-overlap/0835-image-overlap.py
@@ -1,21 +1,43 @@
-class Solution:
-    def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
-        a_1 = []
-        b_1 = []
-        for i in range(len(A)):
-            for j in range(len(A[0])):
-                if A[i][j] == 1:
-                    a_1.append((i,j))
-                if B[i][j] == 1:
-                    b_1.append((i,j))
-        d = {}
-        ans = 0
-        for a_x, a_y in a_1:
-            for b_x,b_y in  b_1:
-                tr = (b_x - a_x, b_y - a_y)
-                if tr in d:
-                    d[tr] += 1
-                else:
-                    d[tr] = 1
-                ans = max(ans, d[tr])
-        return ans
+class Solution(object):
+    def largestOverlap(self, img1, img2):
+        """
+        :type img1: List[List[int]]
+        :type img2: List[List[int]]
+        :rtype: int
+        """
+
+        # return 0 if img1 is empty
+        if len(img1) == 0:
+            return 0
+
+        # create sets of indices that are one 
+        idx1 = set()
+        for i,_ in enumerate(img1):
+            for j,_ in enumerate(img1[i]):
+                if img1[i][j] == 1:
+                    idx1.add((i,j))
+                
+        idx2 = set()
+        for i, _ in enumerate(img2):
+            for j, _ in enumerate(img2[i]):
+                if img2[i][j] == 1:
+                    idx2.add((i,j))
+
+
+        # add the shift to the indices
+        def shift_and_count(shift_hor, shift_ver):
+            shifted_idx1 = set()
+            for (h,v) in idx1:
+                shifted_idx1.add((h+shift_hor,v+shift_ver))
+        
+            # compute the intersection of the shifted set and img2 as this is the number of overlapping ones
+            return len(shifted_idx1.intersection(idx2))
+
+
+        max_value = 0
+
+        for shift_hor in range(-len(img1), len(img1)):
+            for shift_ver in range(-len(img1), len(img1)):
+                max_value = max(max_value, shift_and_count(shift_hor, shift_ver))
+
+        return max_value
