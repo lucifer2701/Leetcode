df87fd6141d2d7cecb63125f15691d4851600330
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-10-30 11:03:45 +0530

    Time: 277 ms (83.23%), Space: 16.3 MB (17.49%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -4,5 +4,28 @@ class Solution:
         for i in nums:
             i=i*i
             ans.append(i)
-        ans.sort()
-        return ans
+        x=min(ans)
+        a=ans.index(x)
+        if a==0:
+            return ans
+        s=ans[:a]
+        s.reverse()
+        t=ans[a:]
+        res=[]
+        while len(s)>0 or len(t)>0:
+            if len(s)>0 and len(t)>0:
+                if s[0]<t[0]:
+                    res.append(s.pop(0))
+                elif s[0]>t[0]:
+                    res.append(t.pop(0))
+                else:
+                    res.append(s.pop(0))
+                    res.append(t.pop(0))
+            else:
+                if len(s)==0 and len(t)==0:
+                    break
+                if len(s)==0 and len(t)>0:
+                    res.append(t.pop(0))
+                if len(t)==0 and len(s)>0:
+                    res.append(s.pop(0))
+        return res
