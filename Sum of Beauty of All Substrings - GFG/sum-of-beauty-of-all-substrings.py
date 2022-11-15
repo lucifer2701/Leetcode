4af6667da0f2679c39776c33c7ebf796fbd1e6ec
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-11-15 16:38:43 +0530

    Added solution - LeetHub

---
+++ Sum of Beauty of All Substrings - GFG/sum-of-beauty-of-all-substrings.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+from collections import Counter
+class Solution:
+    def beautySum(self, s):
+        # Code here
+        ans=0
+        for i in range(len(s)):
+            subStr = ""
+            d={}
+            mf=0;lf=0
+            for j in range(i,len(s)):
+                if s[j] in d:
+                    d[s[j]]+=1
+                    mf=max(mf,d[s[j]])
+                    lf=min(lf,d[s[j]])
+                else:
+                    d[s[j]]=1
+                    mf=max(1,mf)
+                    lf=1
+                lf=min(d.values())
+                ans+=(mf-lf)
+        return ans
+                    
+                
+            
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input()
+        ob = Solution()
+        print(ob.beautySum(s))
+# } Driver Code Ends
