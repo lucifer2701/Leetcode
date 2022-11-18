24425b1e5cf562878f5bf250c26b21b2ea83bc72
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-11-19 00:21:57 +0530

    Added solution - LeetHub

---
+++ Check if all levels of two trees are anagrams or not - GFG/check-if-all-levels-of-two-trees-are-anagrams-or-not.py
@@ -0,0 +1,134 @@
+from typing import Optional
+from collections import deque
+"""
+
+definition of binary tree node.
+class Node:
+    def _init_(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+"""
+
+class Solution:
+    def areAnagrams(self, node1 : Optional['Node'], node2 : Optional['Node']) -> bool:
+        # code here
+        ans1=[];ans2=[]
+        def dfs(node,level):
+            if node:
+                if level>=len(ans1):
+                    ans1.append([])
+                ans1[level].append(node.data)
+                dfs(node.left,level+1)
+                dfs(node.right,level+1)
+        def ddfs(node,level):
+            if node:
+                if level>=len(ans2):
+                    ans2.append([])
+                ans2[level].append(node.data)
+                ddfs(node.left,level+1)
+                ddfs(node.right,level+1)
+        dfs(node1,0)
+        ddfs(node2,0)
+        if len(ans1)!=len(ans2):
+            return 0
+        for i in range(len(ans1)):
+            if ans1[i]!=ans2[i][::-1]:
+                return 0
+        return 1
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+    def __init__(self,val):
+        self.data=val
+        self.right=None
+        self.left=None
+
+# Function to Build Tree
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+
+def inputTree():
+    treeString=input().strip()
+    root = buildTree(treeString)
+    return root
+def inorder(root):
+    if (root == None):
+       return
+    inorder(root.left);
+    print(root.data,end=" ")
+    inorder(root.right)
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        node1 = inputTree();
+        
+        
+        node2 = inputTree();
+        
+        obj = Solution()
+        res = obj.areAnagrams(node1, node2)
+        
+        result_val = 1 if res else 0
+        print(result_val)
+
+# } Driver Code Ends
