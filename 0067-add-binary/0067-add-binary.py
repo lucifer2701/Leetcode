8b22f5f6f01daf11916b1ac5ef34a9234b7ee685
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-11-28 15:56:59 +0530

    Time: 73 ms (26.08%), Space: 13.9 MB (26.02%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,3 +1,2 @@
 class Solution:
-    def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a,2)+int(b,2)).replace('0b','')
+    def addBinary(self, a: str, b: str) -> str: return bin(int(a,2)+int(b,2)).replace('0b','')
