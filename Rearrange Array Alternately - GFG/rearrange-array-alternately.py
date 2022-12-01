bbf0edd2d35bc5ac0ad01c47986b326577a8ae0f
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-12-01 15:05:48 +0530

    Added solution - LeetHub

---
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.py
@@ -0,0 +1,50 @@
+#User function Template for python3
+class Solution:
+    ##Complete this function
+    #Function to rearrange  the array elements alternately.
+    def rearrange(self,arr, n): 
+        ##Your code here
+        temp=arr.copy()
+        temp.sort()
+        x=0;y=n-1
+        for i in range(n):
+            if i%2==0:
+                arr[i]=temp[y]
+                y-=1
+            else:
+                arr[i]=temp[x]
+                x+=1
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            ob=Solution()
+            ob.rearrange(arr,n)
+            
+            for i in arr:
+                print(i,end=" ")
+            
+            print()
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
