8ab7b5dc129e8a8705c9374b05006c282164513f
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-12-01 15:13:59 +0530

    Added solution - LeetHub

--- Rearrange Array Alternately - GFG/rearrange-array-alternately.py
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.py
@@ -4,16 +4,16 @@ class Solution:
     #Function to rearrange  the array elements alternately.
     def rearrange(self,arr, n): 
         ##Your code here
-        temp=arr.copy()
         x=0;y=n-1
-        for i in range(n):
-            if i%2==0:
-                arr[i]=temp[y]
-                y-=1
-            else:
-                arr[i]=temp[x]
-                x+=1
-        
+        m=arr[-1]+1
+        for i in range(0,n,2):
+            arr[i]+=(arr[y]%m)*m
+            y-=1
+        for j in range(1,n,2):
+            arr[j]+=(arr[x]%m)*m
+            x+=1
+        for k in range(n):
+            arr[k]//=m
 
 
 #{ 
