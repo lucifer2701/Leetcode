8292b05ab19329d43a3cbaf807cc2b5d323b5bde
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2022-12-05 10:23:51 +0530

    Added solution - LeetHub

---
+++ Roman Number to Integer - GFG/roman-number-to-integer.py
@@ -0,0 +1,54 @@
+#User function Template for python3
+
+class Solution:
+    def romanToDecimal(self, S): 
+        # code here
+        s=S
+        suma = 0
+        s+="A"
+        
+        for i in range(len(s[:-1])):
+            if s[i]=="M":
+                suma+=1000
+                
+            elif s[i]=="D":
+                suma+=500
+                
+            elif s[i]=="C" and (s[i+1]=="D" or s[i+1]=="M"):
+                suma-=100
+                
+            elif s[i]=="C" and (s[i+1]!="D" or s[i+1]!="M"):
+                suma+=100
+                
+            elif s[i]=="L":
+                suma += 50
+                
+            elif s[i]=="X" and (s[i+1]=="L" or s[i+1]=="C"):
+                suma -= 10
+                
+            elif s[i]=="X" and (s[i+1]!="L" or s[i+1]!="C"):
+                suma += 10
+            
+            elif s[i]=="V":
+                suma +=5
+                
+            elif s[i]=="I" and (s[i+1]=="V" or s[i+1]=="X"):
+                suma -= 1
+                
+            elif s[i]=="I" and (s[i+1]!="V" or s[i+1]!="X"):
+                suma += 1
+            
+        return suma
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t = int(input())
+    for _ in range(t):
+        ob = Solution()
+        S = input()
+        print(ob.romanToDecimal(S))
+# } Driver Code Ends
