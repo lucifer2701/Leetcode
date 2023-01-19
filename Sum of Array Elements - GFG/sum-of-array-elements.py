ae0cbdb0fe75ff990418d7513dbc83d09f4fcd72
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2023-01-19 14:25:58 +0530

    Added solution - LeetHub

---
+++ Sum of Array Elements - GFG/sum-of-array-elements.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+def sumElement(arr,n):
+    ans=0
+    for i in arr:
+        ans+=i
+    return ans
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ == '__main__':
+    testCase=int(input())
+    
+    for _ in range(testCase):
+        n=int(input())
+        arr=[int(x) for x in input().split()]
+        
+        print(sumElement(arr,n))
+# } Driver Code Ends
