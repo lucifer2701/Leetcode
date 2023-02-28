358f48e51b1d9c5ffb0558cb69f511f33db91c40
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2023-02-28 10:28:39 +0530

    Added solution - LeetHub

---
+++ Optimal Array - GFG/optimal-array.py
@@ -0,0 +1,49 @@
+from typing import List
+
+
+class Solution:
+    def optimalArray(self, n : int, a : List[int]) -> List[int]:
+        # code here
+        li=[]
+
+        li.append(0)
+        
+        for i in range(1,n):
+        
+               li.append(li[0]+li[i-1]+abs(a[i]-a[i//2]))
+        
+        return li
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        a=IntArray().Input(n)
+        
+        obj = Solution()
+        res = obj.optimalArray(n, a)
+        
+        IntArray().Print(res)
+        
+
+# } Driver Code Ends
