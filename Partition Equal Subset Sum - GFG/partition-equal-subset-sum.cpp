936fcfef43c52b00ca1b9c6178e7ba6888344521
lucifer2701 <114388726+lucifer2701@users.noreply.github.com>
2023-09-15 11:11:09 +0530

    Added solution - LeetHub

---
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int equalPartition(int n, int arr[])
+    {
+        long long sum = 0;
+        for(int i=0;i<n;i++)sum+=(long long)arr[i];
+        if(sum%2==1) return 0;
+        
+        sum = sum/2;
+        int dp[sum+1] = {1};
+        for(int i=0;i<n;i++){
+            for(int j=sum;j>=arr[i];j--){
+                dp[j] = dp[j-arr[i]] or dp[j];
+            }
+        }
+        
+        return dp[sum];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        if(ob.equalPartition(N, arr))
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
